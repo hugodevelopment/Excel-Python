26c8b26875e267c0c2dbc9cc81745ec74dba7a82
Hugox96 <alveshugo2906@gmail.com>
2022-02-08 19:56:49 -0300

    Grafico de dispersao

--- ExcelPython1.py
+++ ExcelPython1.py
@@ -3,13 +3,13 @@ from re import X
 import pandas as pd
 import matplotlib.pyplot as plt
 
-tabela = pd.read_excel('/content/drive/MyDrive/Python com Excel/ExcelPython3.xlsx') #aqui irá ler os dados em Excel
+tabela = pd.read_excel('/content/drive/MyDrive/Python com Excel/ExcelPython4.xlsx') #aqui irá ler os dados em Excel
 display(tabela) #vamos imprimir a tabelo de um jeito mais bonito
 #print("O valor do índice 3 na coluna base é:", tabela['Base'] [3]) #escolhendo um valor espeficico para ler
 
-plt.hist(tabela["Altura"]) #histograma dos dados
-plt.plot(tabela['Altura'])  #gráfico de linha da altura
-plt.pie(tabela['Idades'],labels= tabela["Nomes"],autopct="%1.0f%%") #gráfico em pizza com legenda e porcentagem
-
+#plt.plot(tabela["Altura"]) #histograma dos dados
+#plt.plot(tabela['Idades']) #gráfico de linha da altura
+#plt.pie(tabela['Idades'],labels= tabela["Nomes"],autopct="%1.0f%%") #gráfico em pizza com legenda e porcentagem
+plt.scatter(tabela['F(gf)'],tabela['l(mm)'],)
 plt.show()
 
