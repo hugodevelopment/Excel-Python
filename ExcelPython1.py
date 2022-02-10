c93eedbd4ec175ba18c586576aa95c31bf45ee89
Hugox96 <alveshugo2906@gmail.com>
2022-02-10 19:05:08 -0300

    Criando diagrama de dispersao

--- ExcelPython1.py
+++ ExcelPython1.py
@@ -10,6 +10,9 @@ display(tabela) #vamos imprimir a tabelo de um jeito mais bonito
 #plt.plot(tabela["Altura"]) #histograma dos dados
 #plt.plot(tabela['Idades']) #gráfico de linha da altura
 #plt.pie(tabela['Idades'],labels= tabela["Nomes"],autopct="%1.0f%%") #gráfico em pizza com legenda e porcentagem
-plt.scatter(tabela['F(gf)'],tabela['l(mm)'],)
+
+plt.scatter(tabela['F(gf)'],tabela['l(mm)'],) #Aqui posso criar um gráfico de dispersão
+plt.xlabel("F(gf)") #Colocando legendas no eixo x
+plt.ylabel("l(mm)") #Colocando legendas no eixo y
 plt.show()
 
