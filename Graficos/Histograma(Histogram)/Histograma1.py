9b9ca6991050a0c30f735189f1a9a717e0500589
Hugox96 <alveshugo2906@gmail.com>
2022-02-24 20:20:05 -0300

    Melhorando histograma

--- Graficos/Histograma(Histogram)/Histograma1.py
+++ Graficos/Histograma(Histogram)/Histograma1.py
@@ -9,8 +9,14 @@ from numpy import std
 
 tabela = pd.read_excel('/content/drive/MyDrive/Python com Excel/ExcelPython3.xlsx') #aqui irá ler os dados em Excel
 display(tabela) #vamos imprimir a tabelo de um jeito mais bonito
-#print("O valor do índice 3 na coluna base é:", tabela['Base'] [3]) #escolhendo um valor espeficico para ler
+#print("O valor do índice 3 na coluna base é:", tabela['Nomes'] [3]) #escolhendo um valor espeficico para ler
 
-plt.hist(tabela["Altura"]) #histograma dos dados
+plt.hist(tabela["Altura"],rwidth=0.9) #histograma dos dados
+# Aqui temos o rwidth que nos mostra o tamanho relativo das barras dos histogramas
+plt.title("Altura dos meus parças", fontsize=20)
+plt.xlabel("Altura", fontsize=15)
+plt.ylabel("Frequência", fontsize=15)
+plt.tick_params(labelsize=10)
+
+print()
 plt.show()
-
