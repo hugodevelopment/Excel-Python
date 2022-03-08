b76ce89a6a1c82d49bd0745055f9b32699c2a66a
Hugox96 <alveshugo2906@gmail.com>
2022-03-08 17:26:29 -0300

    Estudando sobre legendas e grid

--- Graficos/Retas/Retas1.py
+++ Graficos/Retas/Retas1.py
@@ -8,12 +8,23 @@ from numpy import std
 
 #Agora irei estudar como colocar e editar legendas no gráfico para ficar mais bonito e visual
 
-# Neste caso irei plotar a função f(x) = 2x
+#Neste caso irei plotar a função f(x) = 2x
 tabela = pd.read_excel('/content/drive/MyDrive/Python com Excel/ExcelPython5.xlsx') #aqui irá ler os dados em Excel
-display (tabela)
+display(tabela)
 
 plt.figure(figsize=(6,4))
 plt.plot(tabela["x"], tabela["y"])
+
+# Agora irei inserir as legendas nos eixos x e y
+plt.xlabel("x")
+plt.ylabel("f(x) = y")
+plt.grid(True) #Aqui coloco uma linhas de grade
+
+# Vou adicionar uma nova linha no gráfico
+plt.plot(tabela['x/2'])
+
+
+
 
 
 
