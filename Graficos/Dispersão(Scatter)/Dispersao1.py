13b29e8cf0c4cbbadbf8cca4ed4db0316f64b4b1
Hugox96 <alveshugo2906@gmail.com>
2022-06-26 18:32:16 -0300

    melhorando os graficos

--- Graficos/Dispersão(Scatter)/Dispersao1.py
+++ Graficos/Dispersão(Scatter)/Dispersao1.py
@@ -7,12 +7,12 @@ import numpy as np
 from numpy import mean
 from numpy import std
 
-tabela = pd.read_excel('/content/drive/MyDrive/Python com Excel/ExcelPython4.xlsx') #aqui irá ler os dados em Excel
+tabela = pd.read_excel('/content/drive/MyDrive/Python com Excel/Gráfico_de_Dispersão(FG).xlsx') #aqui irá ler os dados em Excel
 display(tabela) #vamos imprimir a tabelo de um jeito mais bonito
 
-# plt.scatter(tabela['F(gf)'],tabela['l(mm)'],) #Aqui posso criar um gráfico de dispersão com os eixos x e y
-# plt.xlabel("F(gf)") #Colocando legendas no eixo x
-# plt.ylabel("l(mm)") #Colocando legendas no eixo y
+plt.scatter(tabela['F(gf)'],tabela['l(mm)'],) #Aqui posso criar um gráfico de dispersão com os eixos x e y
+plt.xlabel("F(gf)") #Colocando legendas no eixo x
+plt.ylabel("l(mm)") #Colocando legendas no eixo y
 
 plt.show()
 
--- Graficos/Retas/inflação.py
+++ Graficos/Retas/inflação.py
@@ -12,8 +12,32 @@ display(tabela) #vamos imprimir a tabela de um jeito mais bonito
 
 plt.figure(figsize=(12,6))
 plt.plot(tabela['Meses'],tabela['Inflação'])
+#Aqui posso criar um gráfico de dispersão com os eixos x e y
+
+from re import X
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from numpy import mean
+from numpy import std
+
+tabela = pd.read_excel('/content/drive/MyDrive/Python com Excel/Infla2020.xlsx') #aqui irá ler os dados em Excel
+display(tabela) #vamos imprimir a tabela de um jeito mais bonito
+
+plt.figure(figsize=(12,6))
+plt.plot(tabela['Meses'],tabela['Inflação'])
 
 #Aqui posso criar um gráfico de dispersão com os eixos x e y
+plt.xlabel('Meses')
+plt.ylabel('inflaçao')
+
+
+
+
+
+
 
 
 
